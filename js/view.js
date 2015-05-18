7f8bd41f704473807cb8b07f7f0f861ca8660e77
Elliott Marquez <marquez@mit.edu>
2015-05-18 04:31:10 -0400

    Gallery Box auto-resize

--- js/view.js
+++ js/view.js
@@ -8,6 +8,49 @@ $(function () {
   var $items = $('.menu .item');
   var $logo  = $('#logo');
 
+  var resizeBoxes = function () {
+    var $images = $('.projects .gallery img');
+
+    $images.each(function() {
+      var $this  = $(this);
+      var src    = $this.attr('src');
+      var tempIm = new Image();
+
+      tempIm.src = src;
+
+      $(tempIm).on('load', function() {
+        var width  = tempIm.width;
+        var height = tempIm.height;
+
+        var wScale = 148 / (0.0 + width);
+        var hScale = 148 / (0.0 + height);
+
+        var scale = 0;
+
+        if (wScale < hScale) {
+          scale = wScale;
+        } else {
+          scale = hScale;
+        }
+
+        var newWidth  = width * scale;
+        var newHeight = height * scale;
+
+        if (newWidth < 148) {
+          var pad = (148 - newWidth)/2;
+
+          $this.css({"padding-left" : pad, "padding-right" : pad});
+        }
+
+        if (newHeight < 148) {
+          var pad = (148 - newHeight)/2;
+
+          $this.css({"padding-top" : pad, "padding-bottom" : pad});
+        }
+      });
+    });
+  }
+
   var navFunction = function () {
     var $this, $bodyContent, newSection, $activeContent, $newContent;
     $this = $(this);
@@ -35,4 +78,5 @@ $(function () {
 
   $items.on('click', navFunction);
   $logo.on('click', function() {$('.menu .item[value="about"]').click()});
+  resizeBoxes();
 });
