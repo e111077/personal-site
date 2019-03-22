e0fb8dbcd4d3c4a3c2e0d2f9a729c7a5a7c18c5c
Elliott Marquez <emarquez@google.com>
2019-03-21 21:09:17 -0700

    I'm dumb it goes in the lib folder

---
+++ src/elliott-main-page.ts
@@ -0,0 +1,10 @@
+import {LitElement, customElement, html} from 'lit-element';
+
+@customElement('elliott-main-page')
+class ElliottMainPage extends LitElement {
+  render() {
+    return html`
+      <div>Hello world</div>
+    `;
+  }
+}
