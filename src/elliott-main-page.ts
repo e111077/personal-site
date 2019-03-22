ee9cf64a527133427f6017dd5e96f00e7db7ad8b
Elliott Marquez <emarquez@google.com>
2019-03-21 22:20:27 -0700

    basic routing and stuff

--- src/elliott-main-page.ts
+++ src/elliott-main-page.ts
@@ -1,10 +1,59 @@
-import {LitElement, customElement, html} from 'lit-element';
+import {LitElement, customElement, html, css, property, TemplateResult} from 'lit-element';
+import {installRouter} from 'pwa-helpers';
+
+import {VIEWS} from './constants.js'
+
+import './elliott-header-bar.js';
+import "./elliott-nav-section";
 
 @customElement('elliott-main-page')
 class ElliottMainPage extends LitElement {
+  @property({type: String})
+  backHref: string = '';
+
+  @property({type: Number})
+  view: number = VIEWS.HOME;
+
+  constructor() {
+    super();
+    installRouter(this.onNavigation)
+  }
+
+  onNavigation = (location: Location) => {
+
+  }
+
+  static get styles() {
+    return css`
+      :host {
+        --main-font: ElliScript, Apple Symbols, BlinkMacSystemFont,
+            "Google Sans", "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
+      }
+    `;
+  }
+
+  renderHomeView(): TemplateResult {
+    return html`
+    `;
+  }
+
+  renderView(): TemplateResult {
+    switch (this.view) {
+      case VIEWS.HOME:
+        return this.renderHomeView();
+      default:
+        return this.renderHomeView();
+    }
+  }
+
   render() {
     return html`
-      <div>Hello world</div>
+      <elliott-header-bar
+          name="Elliott Marquez"
+          backHref=${this.backHref}>
+      </elliott-header-bar>
+      <elliott-nav-section></elliott-nav-section>
+      ${this.renderView()}
     `;
   }
 }
---
+++ src/elliott-nav-section.ts
@@ -0,0 +1,10 @@
+import { customElement, LitElement, html } from "lit-element";
+
+@customElement('elliott-nav-section')
+class ElliottNavSection extends LitElement {
+  render() {
+    return html`
+      <div>NAVIGATIONZZZ</div>
+    `;
+  }
+}
